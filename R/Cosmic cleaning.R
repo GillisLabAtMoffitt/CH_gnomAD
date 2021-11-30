e6b611302873c97655d15d0940877acfcd6d3d47
Christelle Leitzinger <55062754+chrisleitzinger@users.noreply.github.com>
2021-11-30 16:39:21 -0500

    Add normality test, update multimodality test and cosmic wrangling

--- R/Cosmic cleaning.R
+++ R/Cosmic cleaning.R
@@ -10,9 +10,12 @@ head(cosmic_data)
 
 
 # Extract cosmic id, occurrence and occurrence site
-cosmic_data1 <- cosmic_data %>% 
+cosmic_data <- cosmic_data %>% 
   mutate(cosmic_id = str_match(INFO, "ID=(.*?);")[,2]) %>% 
   mutate(occurrence = str_match(INFO, ";OCCURENCE=(.*?)$")[,2]) %>% 
-  mutate(occ_sum = sapply(str_extract_all(cosmic_data1$occurrence, "(\\d)"),
-                          function(x) sum(as.numeric(x))))
-head(cosmic_data1)
+  mutate(occ_sum = sapply(str_extract_all(occurrence, "(\\d)"),
+                          function(x) sum(as.numeric(x)))) %>% 
+  mutate(cosmic_presence = "In Cosmic")
+head(cosmic_data)
+
+write_rds(cosmic_data, "cosmic_data.rds")
