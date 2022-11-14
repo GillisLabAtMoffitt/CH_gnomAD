c57a4643808c1deae3ad13976c2a9a21b72e5889
chrisleitzinger <christelleleitzinger@gmail.com>
2022-11-14 09:48:56 -0500

    Add Hedges g and clean up

--- Terminal HPC/02.age_comparison_pval.sh
+++ Terminal HPC/02.age_comparison_pval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l walltime=120:00:00
+#PBS -l walltime=200:00:00
 #PBS -l nodes=1:ppn=1,pmem=100gb
 module load R/4.0.2
 cd /share/lab_gillis/Christelle/gnomAD_raw_data/splitted_data/age_comparison_pval
