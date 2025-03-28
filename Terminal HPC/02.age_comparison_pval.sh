69e4e6832ee630d27f7828289f60cff81122f451
chrisleitzinger <christelleleitzinger@gmail.com>
2025-03-28 13:57:49 -0400

    clean up

--- Terminal HPC/02.age_comparison_pval.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#PBS -l walltime=200:00:00
-#PBS -l nodes=1:ppn=1,pmem=100gb
-module load R/4.0.2
-cd /share/lab_gillis/Christelle/gnomAD_raw_data/splitted_data/age_comparison_pval
-Rscript age_comparison_pval.R
